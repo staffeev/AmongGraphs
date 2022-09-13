2669820cfcf1eba2bda47e50eb339d3c8c85f587
Ivan <st.affeev@yandex.ru>
2022-09-13 18:30:27 +0300

    group select added

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from functions import get_graph_names, get_graph_by_name
 from PyQt5.Qt import QStandardItemModel, QAbstractItemView
 from PyQt5 import uic
 from PyQt5.QtGui import QCloseEvent, QIcon
+from PyQt5.QtCore import QItemSelectionModel, QModelIndex
 from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
 from models import db_session
 from settings import NOT_OPEN, ENTER_GRAPH
@@ -41,6 +42,7 @@ class Mentor(QMainWindow):
         self.graph_list.setModel(self.treeModel)
         self.graph_list.setSelectionMode(QAbstractItemView.ExtendedSelection)
         self.graph_list.selectionModel().selectionChanged.connect(self.selectFromTree)
+        self.graph_list.clicked.connect(self.test)
         # Настройка относительного позиционирования элементов главного окна
         self.splitter.addWidget(self.canvas)
         self.splitter.setSizes([100, 700])
@@ -50,7 +52,6 @@ class Mentor(QMainWindow):
         self.actionCreate.triggered.connect(self.createGraph)
         self.actionOpen.triggered.connect(self.openGraph)
         self.actionDelete.triggered.connect(self.deleteGraph)
-        # self.actionSave.triggered.connect(self.saveChanges)
         self.actionExit.triggered.connect(self.close)
         self.actionEdit_matrix.triggered.connect(self.openWindow)
         self.actionEdit_list.triggered.connect(self.openWindow)
@@ -136,15 +137,44 @@ class Mentor(QMainWindow):
             self.window.close()
         session.close()
 
-    def saveChanges(self) -> None:
-        """Метод для сохранения изменений"""
-        # TODO: save changes
-        pass
+
+    def test(self):
+        return
+        model = index.model()
+        item = model.itemFromIndex()
+        print(item.text())
+
+    def iterTree(self):
+        return
+        model = self.graph_list.model()
+        # model = QStandardItemModel()
+        for i in range(model.rowCount()):
+            item = model.item(i)
+            print(item.text())
+            for j in range(item.rowCount()):
+                item_child = item.child(j, 0)
+                print('\t' + item_child.text())
+
+    def selectChildren(self, index: QModelIndex):
+        """Выделение всех детей поддерева"""
+        selection_model = self.graph_list.selectionModel()
+        model = index.model()
+        item = model.itemFromIndex(index)
+        for i in range(item.rowCount()):
+            child = model.index(i, 0, index)
+            selection_model.select(child, QItemSelectionModel.Select)
 
     def selectFromTree(self) -> None:
         """Выделение тех элементов на холсте, которые были выделены в дереве"""
         self.canvas.unselect()
-        names = {i.model().itemFromIndex(i).text() for i in self.graph_list.selectedIndexes()}
+        names = set()
+        for ix in self.graph_list.selectedIndexes():
+            item = ix.model().itemFromIndex(ix)
+            parent = ix.parent().row()
+            if parent == -1:  # Root node
+                self.selectChildren(ix)
+                continue
+            names.add(item.text())
         self.canvas.select(names)
 
     def showTreeOfElements(self) -> None:
@@ -160,6 +190,7 @@ class Mentor(QMainWindow):
         ribs = TreeItem("Ribs")
         ribs.appendRows([TreeItem(str(r), 8) for r in graph.ribs.values()])
         self.rootNode.appendRows([nodes, ribs])
+        self.iterTree()
         session.close()
 
     def clearTree(self) -> None:
